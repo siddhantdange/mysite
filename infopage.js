35a86b23ae64c101ef91d846a21d15e280552a69
siddhantdanger <siddhantdanger@gmail.com>
2013-05-20 10:46:26 -0700

    added content to 'home' and 'me' page-slides

--- infopage.js
+++ infopage.js
@@ -3,4 +3,9 @@ $(document).ready(function(){
 	$('#tablecontainer #tablelist > ul li').click(function(){
 		$('#tablecontainer > #tabledetail > #tabledetailpage').html($('div#' + $(this).attr('id')).html());
 	});
+	
+	$('.clickable').click(function(){
+		alert('here');
+		loadPageSlide('/' + $(this).attr('id') + '.html');
+	});
 });
