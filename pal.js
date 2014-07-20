0ab1d4f3c68b116cce212386b94cd28008bdc1b5
Sid Dange <dange.siddhant@gmail.com>
2014-07-20 00:18:37 -0700

    added code for pal email verification

---
+++ pal.js
@@ -0,0 +1,10 @@
+xmlhttp.open("POST"," https://api.parse.com/1/functions/confirmEmail",true);
+xmlhttp.setRequestHeader("Content-type","application/json");
+xmlhttp.setRequestHeader("X-Parse-Application-Id", "amBfyHfkqcjWSIH1tteESSpiHI3Z
+Ekz3Xuk0eGNF");
+xmlhttp.setRequestHeader("X-Parse-REST-API-Key", "UWwg3Jp7lldJEK3MSTKbYehWW0VBbtDhM8c2TSW6");
+xmlhttp.setRequestHeader("X-Parse-Session-Token", "xwVz5ygnJguN3j6Z29bFyHlsX");
+xmlhttp.setRequestHeader(key, value);
+
+xmlhttp.send("access_token", "haha");
+
